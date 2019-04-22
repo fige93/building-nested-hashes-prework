3479f0f0f37851470ec5b16f6f87f9f0b5c1c273
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-22 19:43:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,11 @@ def third_challenge
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
+   :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
+
    :capulet => {
       :patriarch => {
         name: "Lord Capulet", age: "50"},
